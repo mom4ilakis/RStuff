67678e72f4f394cec7949620364224cce43b3003
Momchil Marinov <mom4ilakis@gmail.com>
2019-11-28 10:57:40 +0200

    init commit

--- lab7.R
+++ lab7.R
@@ -1,70 +1,31 @@
-getNorm = function(n, mean, disp){
-  
-  return(rnorm(n,mean,sqrt(disp)));
+#t.test for normal
+#wilcox for not normal
+#prop.test for propotional
+# t.test(vacation, mu = 24, alternative = 'two.sided')
+#> t = (mn - 24)*sqrt(35) / standartDiv
+#> mn = mean(vacation)
+#> standartDiv = sd(vacation)
+#> pt(t, 34) * 2
+#> prop.test(41, 100, 0.5, alternative = 'two.sided') can't discard hypo2^
+#> prop.test(410, 1000, 0.5, alternative = 'two.sided') discard hyp
+#> wilcox.test(data,mu=5,alternative = 'greater')
+#> wilcox.test(stomach, mu = 100, alternative = 'less')
+#> t = table(Smoke[Sex == 'Male'])
+#> numberNon = t['Never']
+#> all = sum(t)
+
+#> prop.test(numberNon, all, p=0.81, alternative = 'less')
+
+
+genNormal = function (n , mean, sd ) {
+  return(rnorm(n,mean,sqrt(sd)));
 }
-
-#> xTask1 = getNorm(20,3,4);
-#> mean(XTask1);
-
-getQuantillnorm = function(p,mean,disp){
-  
-  return(qnorm(p,mean,sqrt(disp)));
-}
-
-#> q = getQunatillNorm(0.957,0,1)
-
-#calculates the "trust" interval end points
-calcLimitPoints = function(q,n,mean,disp){
-  return(c(mean-q*sqrt(disp/n),mean+q*sqrt(disp/n)));
-}
-
-#t.test if nothing given exept data gives trust interal end point calc at 95%
-
-calcTrustEndPoints = function(data,trustLevel=0.957){
-  return(t.test(data,conf.level=trustLevel));
-}
-
-# copy data from pdf in moodle
-#use scan when there are no commas, name_var = scan(), then paste, when done press enter to finish
-#check for normal data with shapiro p-val > 5%(0.05) => norm
-#to get expectation factor will need Ex = mean/ summary
-#test for median wilcox.test
-
-# use boxplot, hist qqnorm qqline and shapiro.test to check if data is normal
-#> qqnorm(rat)
-#> qqline(rat)
-#> shapiro.test(rat)
-#> t.test(rat,conf.level=0.967)
-
-#need to explictily give the conf.int argument
-#>wilcox.test(malpract, conf.int = 0.96)
+#> x = genNormal(10,2,4)
+#> t.test(x,mu=3)
+#> t.test(x,mu=5)
+#> x = genNormal(100,2,4)
+#> t.test(x,mu=3)
+#> t.test(x,mu=5)
 
 
-#> t.test(log(malpract))
-
-
-#prop.test to get confidance(trust) interval
-
-#> prop.test(87,150,conf.level = 0x = getNorm(20,5,4)
-
-
-#> x = getNorm(20,5,4)
-#> t.test(x, conf.level = 0.92)
-
-meanIsIn = function(attempts, n, mean, disp){
-  countMean = 0;
-  
-  for(i in c(1:attempts)){
-    x = getNorm(n, mean, disp);
-    test = t.test(x,conf.level = 0.90);
-    if(mean < test$conf.int[2] && mean > test$conf.int[1]){
-      countMean= countMean + 1;
-    }
-  }
-  
-  return(countMean)
-  
-}
-
-#matplot takex two matrixes X,Y, takes two entries from matrix one as a point and draws a line to a point with coordinates form the second matrix
 
